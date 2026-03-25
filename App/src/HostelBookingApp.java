a6f7deaa22d069468c035fb04b53f94074b4696e
Ankit-Mandal006 <mandal.ankit190506@gmail.com>
2026-03-25 10:04:03 +0530

    Use Case 1: Application Entry & Welcome Message

---
+++ App/src/HostelBookingApp.java
@@ -0,0 +1,12 @@
+/**
+ * @author Ankit Mandal
+ * @version 1.0
+ */
+
+public class HostelBookingApp {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to the Hostel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
+}
